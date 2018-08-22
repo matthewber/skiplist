a9f0b66ec4447fb149c0aaed4f41ed8029fa3083
Matthew Berntsen <matthewberntsen@Matthews-MacBook-Air-2.local>
2018-08-21 19:06:19 -0700

    adding implementation

--- SkipList.java
+++ SkipList.java
@@ -1,16 +1,35 @@
 public class SkipList{
 
-  public SkipListNode topHead;
+  public SkipListNode<String> head;
 
-  public SkipList(SkipListNode head){
-    this.topHead = head;
+  public SkipList(SkipListNode<String> head){
+    this.head = head;
   }
 
   public SkipList(){
     this(null);
   }
 
-  public static void main(String[] args){
+  public boolean isEmpty(){
+    if(head == null){
+      return true;
+    }
+    return false;
+  }
 
+  public void insert(SkipListNode<String> node){
+    if(isEmpty()){
+      this.head = node;
+    }else{
+      //add insert implementation
+    }
+  }
+
+  public static void main(String[] args){
+    SkipList test = new SkipList();
+    SkipListNode<String> testNode = new SkipListNode<String>("test pass", null, null, null, null);
+    test.insert(testNode);
+    boolean empt = test.isEmpty();
+    System.out.println(empt);
   }
 }
--- SkipListNode.java
+++ SkipListNode.java
@@ -1,15 +1,25 @@
-public class SkipListNode{
+public class SkipListNode <E>{
 
   public SkipListNode left;
   public SkipListNode right;
   public SkipListNode up;
   public SkipListNode down;
+  public E item;
 
-  public SkipListNode(SkipListNode left, SkipListNode right, SkipListNode up, SkipListNode down){
+  public SkipListNode(E item, SkipListNode left, SkipListNode right, SkipListNode up, SkipListNode down){
+    this.item = item;
     this.left = left;
     this.right = right;
     this.up = up;
     this.down = down;
+  }
+
+  public SkipListNode(){
+    this(null, null, null, null, null);
+  }
+
+  public static void main(String[] args){
+    SkipListNode test = new SkipListNode();
   }
 
 }
